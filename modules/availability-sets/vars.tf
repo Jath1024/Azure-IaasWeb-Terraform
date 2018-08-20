36cba734c9450584c30c22b8b77990715246302f
James Thompson <jtbb58@googlemail.com>
2018-08-20 15:49:14 +0100

    updates

--- modules/availability-sets/vars.tf
+++ modules/availability-sets/vars.tf
@@ -1,12 +1,15 @@
 # Availability Set Vars
 variable "availability_set_name" {
   description = "name of availability set"
+  default = "iaw-as"
 }
 variable "location" {
-  
-}
-variable "resource_group_name" {
-  
-}
+  description = "Location for deployment"
+  default = "westeurope"
+  }
 
+variable "resource_group_name" {
+  description = "Resource Group name"
+  default = "iaaswebapp"
+  }
 

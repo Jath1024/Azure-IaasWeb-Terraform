721cd3c48cf05aa14504e0a84d00aad67f9dcb79
James Thompson <jtbb58@googlemail.com>
2018-08-20 15:36:44 +0100

    updates

---
+++ modules/web-subnet/vars.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Location for deployment"
+  default = "westeurope"
+  }
+
+variable "resource_group_name" {
+  description = "Resource Group name"
+  default = "iaaswebapp"
+  }
